f36ef504ea4c074d159e194a97bd5465206188e8
moto_moto <ustinovilia34@gmail.com>
2025-05-31 05:03:24 +0300

    feat: updated constatns

--- backend/src/constants.py
+++ backend/src/constants.py
@@ -1,28 +1,23 @@
 from services.llm_interface import LlamaModelInterface
 from src.pocketbase import Pocketbase
+from pathlib import Path
+import sys
 
-LLAMA_MODEL_PATH = "assets/models/Mistral-7B-Instruct-v0.3.Q4_K_S.gguf"
+# Получаем базовую директорию
+if getattr(sys, "frozen", False):
+    # Если запущено как exe
+    base_dir = Path(sys.executable).parent
+else:
+    # Если запущено как скрипт
+    base_dir = Path(__file__).parent.parent
+
+LLAMA_MODEL_PATH = str(
+    base_dir / "assets" / "models" / "Mistral-7B-Instruct-v0.3.Q4_K_S.gguf"
+)
 POCKETBASE_URL = "http://127.0.0.1:8090"
 
 LLAMA_INTERFACE = LlamaModelInterface(model_path=LLAMA_MODEL_PATH)
 PB = Pocketbase(POCKETBASE_URL)
-
-# Настройки аутентификации
-SECRET_KEY = "your-secret-key"  # В продакшене использовать безопасный ключ
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-# Сообщения об ошибках
-ERROR_MESSAGES = {
-    "not_found": "Запись не найдена",
-    "already_exists": "Запись уже существует",
-    "invalid_credentials": "Неверные учетные данные",
-    "unauthorized": "Требуется аутентификация",
-    "forbidden": "Доступ запрещен",
-    "validation_error": "Ошибка валидации данных",
-    "database_error": "Ошибка базы данных",
-    "server_error": "Внутренняя ошибка сервера",
-}
 
 
 class PocketbaseCollections:
